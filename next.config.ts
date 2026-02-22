6f15e7f032a91ecd88de4dc73f854849dd216a23
h-ryushin <115770963+h-ryushin@users.noreply.github.com>
2026-02-23 01:53:44 +0900

    Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  // output: 'export',
   images: {
     unoptimized: true, // GitHub PagesではNext.jsの画像最適化が使えないため必須
   },
-  basePath: '/my-portfolio',
+  // basePath: '/my-portfolio',
 };
 
 export default nextConfig;
