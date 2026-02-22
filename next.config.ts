3208c07bb8a422bf1d4eaf32522913695f2356e7
h-ryushin <115770963+h-ryushin@users.noreply.github.com>
2026-02-23 01:29:24 +0900

    gitHubPagesせってい

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true, // GitHub PagesではNext.jsの画像最適化が使えないため必須
+  },
+  basePath: '/my-portfolio',
 };
 
 export default nextConfig;
