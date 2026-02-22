dea8ed5b21804ee466d71dce8e2e2042c962d8c3
h-ryushin <115770963+h-ryushin@users.noreply.github.com>
2026-02-23 01:34:25 +0900

    公開設定

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   images: {
     unoptimized: true, // GitHub PagesではNext.jsの画像最適化が使えないため必須
   },
