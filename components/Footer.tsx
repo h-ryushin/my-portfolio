1e604669dc04d8508fd9aebaa5819ec1f6c08f98
h-ryushin <115770963+h-ryushin@users.noreply.github.com>
2026-04-14 19:23:36 +0900

    フッター修正

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='w-full h-100 bg-black text-white py-12 flex justify-center items-center mt-20'>
+    <div className='w-full h-40 md:h-100 bg-black text-white py-12 flex justify-center items-center mt-20'>
         <p className='text-base tracking-[0.3em] font-light'>
             © 2026 HIRANO RYUSHIN</p>
     </div>
